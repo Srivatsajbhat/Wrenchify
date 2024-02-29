c39742536f12d30ba998a4ea7ffbfa09a8e02050
Gagan-Dheep <gaganshetty5566@outlook.com>
2024-02-29 20:58:08 +0530

    Rest api to connect from frontend to backend and updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,24 +3,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/usersRoute');
-
-const app = express();
-
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const app = express(); 
+ 
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
-// Connect to MongoDB database
+// Connect to MongoDB database 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true 
 }).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
+    console.log('Connected to MongoDB'); 
+}).catch((error) => { 
     console.error('Error connecting to MongoDB:', error);
 });
 
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+   
